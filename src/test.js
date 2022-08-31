06e272e2d8ed386011b4f814d65b755e514f8c11
Tom Adler <me@arty.name>
2022-08-31 10:44:56 +0200

    replace tape with node:test (#46)

--- src/test.js
+++ src/test.js
@@ -1,12 +1,13 @@
-import test from 'tape'
+import test from 'node:test';
+import { strict as assert } from 'node:assert';
+
 import isMartianIP from './index.js'
 
-test('Martian CIDR', function(t) {
-  t.equal(isMartianIP('8.8.8.8'), false, 'should return false for public IPv4 addresses')
-  t.equal(isMartianIP('2a00:1450:4001:80b::1000'), false, 'should return false for public IPv6 addresses')
-  t.equal(isMartianIP('10.0.0.1'), true, 'should return true for private IPv4 addresses')
-  t.equal(isMartianIP('fe80::1'), true, 'should return true for private IPv6 addresses')
-  t.equal(isMartianIP('ff0e::1'), false, 'should return false for IPv6 addresses from ff0e::/16 subnet')
-  t.throws(() => isMartianIP('not a real IP'), 'should throw for invalid addresses')
-  t.end()
+test('Martian CIDR', () => {
+  assert.equal(isMartianIP('8.8.8.8'), false, 'should return false for public IPv4 addresses')
+  assert.equal(isMartianIP('2a00:1450:4001:80b::1000'), false, 'should return false for public IPv6 addresses')
+  assert.equal(isMartianIP('10.0.0.1'), true, 'should return true for private IPv4 addresses')
+  assert.equal(isMartianIP('fe80::1'), true, 'should return true for private IPv6 addresses')
+  assert.equal(isMartianIP('ff0e::1'), false, 'should return false for IPv6 addresses from ff0e::/16 subnet')
+  assert.throws(() => isMartianIP('not a real IP'), 'should throw for invalid addresses')
 })
